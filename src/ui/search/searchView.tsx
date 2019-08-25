c6be734a30ecf69d69475e7f324a01cde6a04abc
Stefan Schermann <stefan.schermann@gmail.com>
2019-08-25 21:05:03 +0200

    Fix advanced search options

--- src/ui/search/searchView.tsx
+++ src/ui/search/searchView.tsx
@@ -70,7 +70,7 @@ export function renderSearchForm(ctrl: ISearchCtrl) {
         {renderSelectRow(ctrl, i18n('winner'), !!players.length, { name: 'players.winner', options: players})}
         {renderSelectRow(ctrl, i18n('ratingRange'), true, { name: 'ratingMin', options: ratingOptions, placeholder: i18n('from')}, {name: 'ratingMax', options: ratingOptions, placeholder: i18n('to')})}
         {renderSelectRow(ctrl, i18n('opponent'), true, { name: 'hasAi', options: opponentOptions() })}
-        {renderSelectRow(ctrl, i18n('aiNameLevelAiLevel', 'A.I.', '').trim(), isComputerOpp, {name: 'aiLevelMin', options: aiLevelOptions, placeholder: i18n('from')}, { name: 'aiLevelMax', options: aiLevelOptions, placeholder: i18n('to')})}
+        {renderSelectRow(ctrl, i18n('aiNameLevelAiLevel', 'A.I.', '').trim(), isComputerOpp, {name: 'aiLevelMin', options: aiLevelOptions(), placeholder: i18n('from')}, { name: 'aiLevelMax', options: aiLevelOptions(), placeholder: i18n('to')})}
         {renderSelectRow(ctrl, i18n('source'), true, { name: 'source', options: sourceOptions})}
         {renderSelectRow(ctrl, i18n('variant'), true, { name: 'perf', options: perfOptions})}
         {renderSelectRow(ctrl, 'Turns', true, { name: 'turnsMin', options: turnOptions, placeholder: i18n('from')}, {name: 'turnsMax', options: turnOptions, placeholder: i18n('to')})}
@@ -78,7 +78,7 @@ export function renderSearchForm(ctrl: ISearchCtrl) {
         {renderSelectRow(ctrl, i18n('time'), true, { name: 'clock.initMin', options: timeOptions, placeholder: i18n('from')}, {name: 'clock.initMax', options: timeOptions, placeholder: i18n('to')})}
         {renderSelectRow(ctrl, i18n('increment'), true, { name: 'clock.incMin', options: incrementOptions, placeholder: i18n('from')}, {name: 'clock.incMax', options: incrementOptions, placeholder: i18n('to')})}
         {renderSelectRow(ctrl, i18n('result'), true, { name: 'status', options: resultOptions})}
-        {renderSelectRow(ctrl, i18n('winner'), true, {name: 'winnerColor', options: winnerOptions})}
+        {renderSelectRow(ctrl, i18n('winner'), true, {name: 'winnerColor', options: winnerOptions()})}
         {renderSelectRow(ctrl, i18n('date'), true, { name: 'dateMin', options: dateOptions, placeholder: i18n('from'), noEmpty: true }, {name: 'dateMax', options: dateOptions, placeholder: i18n('to')})}
         {renderSelectRow(ctrl, i18n('sort'), true, { name: 'sort.field', options: sortFieldOptions(), noEmpty: true }, {name: 'sort.order', options: sortOrderOptions(), noEmpty: true })}
         <div className="game_search_row">
@@ -207,12 +207,12 @@ const searchOpts = {
   ratings: ['800', '900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900', '2000', '2100', '2200', '2300', '2400', '2500', '2600', '2700', '2800', '2900'],
   aiLevels: ['1', '2', '3', '4', '5', '6', '7', '8'],
   sources: [['1', 'Lobby'], ['2', 'Friend'], ['3', 'Ai'], ['6', 'Position'], ['7', 'Import'], ['5', 'Tournament'], ['10', 'Simul'], ['12', 'Pool']],
-  perfs: [['0', 'UltraBullet'], ['1', 'Bullet'], ['2', 'Blitz'], ['6', 'Rapid'], ['3', 'Classical'], ['4', 'Correspondence'], ['18', 'Crazyhouse'], ['11', 'Chess960'], ['12', 'King of the Hill'], ['15', 'Three-check'], ['13', 'Anticheck'], ['14', 'Atomic'], ['16', 'Horde'], ['17', 'Racing Kings']],
+  perfs: [['0', 'UltraBullet'], ['1', 'Bullet'], ['2', 'Blitz'], ['6', 'Rapid'], ['3', 'Classical'], ['4', 'Correspondence'], ['11', 'Frisian'], ['16', 'Frysk!'], ['13', 'Antidraughts'], ['17', 'Breakthrough']],
   turns: ['1', '2', '3', '4', '5', '10', '15', '20', '25', '30', '35', '40', '45', '50', '60', '70', '80', '90', '100', '125', '150', '175', '200', '225', '250', '275', '300'],
   durations: [['30', '30 seconds'], ['60', '1 minute'], ['120', '2 minutes'], ['300', '5 minutes'], ['600', '10 minutes'], ['900', '15 minutes'], ['1200', '20 minutes'], ['1800', '30 minutes'], ['3600', '1 hour'], ['10800', '3 hours'], ['86400', '1 day'], ['259200', '3 days'], ['604800', '1 week'], ['1209600', '2 weeks'], ['2592000', '1 month'], ['7776000', '3 months'], ['15552000', '6 months'], ['31536000', '1 year']],
   times: [['0', '0 seconds'], ['30', '30 seconds'], ['45', '45 seconds'], ['60', '1 minute'], ['120', '2 minutes'], ['180', '3 minutes'], ['300', '5 minutes'], ['600', '10 minutes'], ['900', '15 minutes'], ['1200', '20 minutes'], ['1800', '30 minutes'], ['2700', '45 minutes'], ['3600', '60 minutes'], ['5400', '90 minutes'], ['7200', '120 minutes'], ['9000', '150 minutes'], ['10800', '180 minutes']],
   increments: [['0', '0 seconds'], ['1', '1 second'], ['2', '2 seconds'], ['3', '3 seconds'], ['5', '5 seconds'], ['10', '10 seconds'], ['15', '15 seconds'], ['30', '30 seconds'], ['45', '45 seconds'], ['60', '60 seconds'], ['90', '90 seconds'], ['120', '120 seconds'], ['150', '150 seconds'], ['180', '180 seconds']],
-  results: [['30', 'Mate'], ['31', 'Resign'], ['34', 'Draw'], ['35', 'Clock Flag'], ['60', 'Variant End']],
+  results: [['30', 'Win'], ['31', 'Resign'], ['34', 'Draw'], ['35', 'Clock Flag'], ['60', 'Variant End']],
   winners: [['1', 'white'], ['2', 'black'], ['3', 'none']],
   dates: [['0d', 'Now'], ['1h', '1 hour ago'], ['2h', '2 hours ago'], ['6h', '6 hours ago'], ['1d', '1 day ago'], ['2d', '2 days ago'], ['3d', '3 days ago'], ['4d', '4 days ago'], ['5d', '5 days ago'], ['6d', '6 days ago'], ['1w', '1 week ago'], ['2w', '2 weeks ago'], ['3w', '3 weeks ago'], ['4w', '4 weeks ago'], ['5w', '5 weeks ago'], ['6w', '6 weeks ago'], ['1m', '1 month ago'], ['2m', '2 months ago'], ['3m', '3 months ago'], ['4m', '4 months ago'], ['5m', '5 months ago'], ['6m', '6 months ago'], ['1y', '1 year ago'], ['2y', '2 years ago'], ['3y', '3 years ago'], ['4y', '4 years ago'], ['5y', '5 years ago']],
   sortFields: [['d', 'date'], ['t', 'numberOfTurns'], ['a', 'averageElo']],
@@ -222,7 +222,7 @@ const searchOpts = {
 const ratingOptions = searchOpts.ratings.map((a: string) => ({value: a, label: a}))
 const opponents = [['0', 'human'], ['1', 'computer']]
 const opponentOptions = () => opponents.map((a: Array<string>) => ({value: a[0], label: i18n(a[1])}))
-const aiLevelOptions = searchOpts.aiLevels.map((a: string) => ({value: a, label: i18n('level') + ' ' + a}))
+const aiLevelOptions = () => searchOpts.aiLevels.map((a: string) => ({value: a, label: i18n('level') + ' ' + a}))
 const sourceOptions = searchOpts.sources.map((a: Array<string>) => ({value: a[0], label: a[1]}))
 const perfOptions = searchOpts.perfs.map((a: Array<string>) => ({value: a[0], label: a[1]}))
 const turnOptions = searchOpts.turns.map((a: string) => ({value: a, label: a}))
@@ -230,7 +230,7 @@ const durationOptions = searchOpts.durations.map((a: Array<string>) => ({value: 
 const timeOptions = searchOpts.times.map((a: Array<string>) => ({value: a[0], label: a[1]}))
 const incrementOptions = searchOpts.increments.map((a: Array<string>) => ({value: a[0], label: a[1]}))
 const resultOptions = searchOpts.results.map((a: Array<string>) => ({value: a[0], label: a[1]}))
-const winnerOptions = searchOpts.winners.map((a: Array<string>) => ({value: a[0], label: i18n(a[1])}))
+const winnerOptions = () => searchOpts.winners.map((a: Array<string>) => ({value: a[0], label: i18n(a[1])}))
 const dateOptions = searchOpts.dates.map((a: Array<string>) => ({value: a[0], label: a[1]}))
 const sortFieldOptions = () => searchOpts.sortFields.map((a: Array<string>) => ({value: a[0], label: i18n(a[1])}))
 const sortOrderOptions = () => searchOpts.sortOrders.map((a: Array<string>) => ({value: a[0], label: i18n(a[1])}))
