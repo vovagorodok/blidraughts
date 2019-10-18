56339cf0335975e78eb623f244e676b606b8328e
Stefan Schermann <stefan.schermann@gmail.com>
2019-10-18 16:40:15 +0200

    Add lidraughts.org about page

--- src/ui/about/index.tsx
+++ src/ui/about/index.tsx
@@ -19,7 +19,11 @@ export default {
       <div class="aboutBody native_scroller">
         <p>lidraughts.org is a free, open-source draughts server powered by volunteers.</p>
 
-        <p>Todo...</p>
+        <p>Chess websites are around abundantly anno 2018, with many powerful features to play, learn and interact (all for free on {externalLink('lichess.org', 'https://lichess.org')}). The surprising absence of such a website for international draughts was an important motivation to create Lidraughts. Founded on the love for draughts, it hopes to grow into a place where people from all over the world can come to enjoy this beautiful game, for free.</p>
+
+        <p>A more personal motivation behind lidraughts.org is to remember our friend and teacher {externalLink('Tjalling Goedemoed', 'https://lidraughts.org/in-memoriam')}, who passed away much too young in 2016. Few have contributed as much to the game of draughts as he did, as an inspiring teacher, problems composer, writer of articles and books, and of course a very strong player. May this website be seen as a tribute to his work!</p>
+
+        <p>Many words of gratitude must be expressed towards Thibault Duplessis for his brilliant piece of work {externalLink('lichess.org', 'https://lichess.org')}. As the astute reader may have noticed, lidraughts.org bears a lot of resemblance to this website, both in name and appearance. Although the two are not formerly affiliated, it is safe to say that lidraughts would not be here today without the {externalLink('source code', 'https://github.com/ornicar/lila')} of lichess as a basis. In its first incarnation, lidraughts.org uses the same framework as lichess.org, reimplemented for the game of draughts. So a big word of thanks to Thibault, the other {externalLink('lichess developers', 'https://github.com/ornicar/lila/graphs/contributors')}, and the open source community in general!</p>
 
         <h2>Links</h2>
 
