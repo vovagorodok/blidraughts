0d23f856cc5e4ed483c652f2914e0f48ef322130
Vincent Velociter <vincent.velociter@gmail.com>
2019-11-06 11:52:54 +0100

    More translations

--- src/ui/analyse/analyseSettings.ts
+++ src/ui/analyse/analyseSettings.ts
@@ -138,7 +138,7 @@ function renderAnalyseSettings(ctrl: AnalyseCtrl) {
     ]),
     ctrl.ceval.allowed ? h('div.action', [
       formWidgets.renderCheckbox(
-        i18n('enableLocalComputerEvaluation'), 'allowCeval', settings.analyse.enableCeval,
+        i18n('toggleLocalEvaluation'), 'allowCeval', settings.analyse.enableCeval,
         v => {
           ctrl.ceval.toggle()
           if (v) ctrl.initCeval()
@@ -175,13 +175,13 @@ function renderAnalyseSettings(ctrl: AnalyseCtrl) {
     ]) : null,
     /*ctrl.ceval.allowed ? h('div.action', [
       formWidgets.renderSlider(
-        'Analysis lines', 'ceval.multipv', 1, 5, 1, settings.analyse.cevalMultiPvs,
+        i18n('multipleLines'), 'ceval.multipv', 1, 5, 1, settings.analyse.cevalMultiPvs,
         ctrl.settings.cevalSetMultiPv
       )
     ]) : null,*/
     ctrl.ceval.allowed && cores > 1 ? h('div.action', [
       formWidgets.renderSlider(
-        'Processor cores', 'ceval.cores', 1, cores, 1, settings.analyse.cevalCores,
+        i18n('cpus'), 'ceval.cores', 1, cores, 1, settings.analyse.cevalCores,
         ctrl.settings.cevalSetCores
       )
     ]) : null
