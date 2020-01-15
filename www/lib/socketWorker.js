49842daa98168af3a98f46d193491a8589b6093a
Stefan Schermann <stefan.schermann@gmail.com>
2020-01-15 22:37:24 +0100

    Revert "Undo null ping for server compatibility"
    This reverts commit 9362c5a197f4111791b0046e1c731af5c394a626.

--- www/lib/socketWorker.js
+++ www/lib/socketWorker.js
@@ -76,6 +76,7 @@ StrongSocket.prototype = {
       self.ackable.resend();
     };
     self.ws.onmessage = function(e) {
+      if (e.data == 0) return self.pong();
       var msg = JSON.parse(e.data);
       var mData = msg.d || [];
 
@@ -145,8 +146,12 @@ StrongSocket.prototype = {
     var self = this;
     clearTimeout(self.pingSchedule);
     clearTimeout(self.connectSchedule);
+    var pingData = (self.options.isAuth && self.pongCount % 8 == 2) ? JSON.stringify({
+      t: 'p',
+      l: Math.round(0.1 * self.averageLag)
+    }) : null;
     try {
-      self.ws.send(self.pingData());
+      self.ws.send(pingData);
       self.lastPingTime = Date.now();
     } catch (e) {
       self.debug(e, true);
@@ -165,16 +170,6 @@ StrongSocket.prototype = {
     // Average first 4 pings, then switch to decaying average.
     var mix = self.pongCount > 4 ? 0.1 : (1 / self.pongCount);
     self.averageLag += mix * (self.currentLag - self.averageLag);
-  },
-
-  pingData: function() {
-    var self = this;
-    var data = {
-      t: 'p'
-    };
-    if (self.version !== undefined) data.v = self.version
-    if (self.pongCount % 8 === 2) data.l = Math.round(0.1 * self.averageLag);
-    return JSON.stringify(data);
   },
 
   handle: function(msg) {
