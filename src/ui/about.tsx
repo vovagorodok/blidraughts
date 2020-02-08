762ae01b9755e1654b34aef1e462b5d23483757d
Vincent Velociter <vincent.velociter@gmail.com>
2020-02-08 22:55:28 +0100

    Remove patron links on ios

--- src/ui/about.tsx
+++ src/ui/about.tsx
@@ -33,7 +33,6 @@ export default {
           <li>{externalLink('Contact', 'https://lidraughts.org/contact')}</li>
           <li>{externalLink('Terms of Service', 'https://lidraughts.org/terms-of-service')}</li>
           <li>{externalLink('Privacy Policy', 'https://lidraughts.org/privacy')}</li>
-          <li>{externalLink('lidraughts.org/about', 'https://lidraughts.org/about')}</li>
         </ul>
 
       </div>
