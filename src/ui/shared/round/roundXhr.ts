a7158f8f6bbee4c77076736195e90c3e054e7b37
Vincent Velociter <vincent.velociter@gmail.com>
2020-02-12 10:40:12 +0100

    Change pgn xhr

--- src/ui/shared/round/roundXhr.ts
+++ src/ui/shared/round/roundXhr.ts
@@ -1,5 +1,5 @@
 import { Plugins } from '@capacitor/core'
-import { fetchJSON, fetchText } from '../../../http'
+import { SESSION_ID_KEY, fetchJSON, fetchText } from '../../../http'
 import { serializeQueryParameters } from '../../../utils'
 import i18n from '../../../i18n'
 import { OnlineGameData } from '../../../lidraughts/interfaces/game'
@@ -13,7 +13,14 @@ export function reload(ctrl: OnlineRoundInterface): Promise<OnlineGameData> {
 
 export function getPDN(gameId: string, algebraic: boolean, raw = false) {
   const params = (raw ? '?evals=0&clocks=0' : '?literate=1') + '&algebraic=' + (algebraic ? '1' : '0')
-  return fetchText(`/game/export/${gameId}${params}`, undefined, true)
+  return fetchText(`/game/export/${gameId}${params}`, {
+    headers: {
+      'Accept': 'application/x-draughts-pdn, text/*',
+      'X-Requested-With': '__delete',
+      [SESSION_ID_KEY]: '__delete',
+    },
+    credentials: 'omit',
+  }, true)
 }
 
 export function readNote(gameId: string) {
