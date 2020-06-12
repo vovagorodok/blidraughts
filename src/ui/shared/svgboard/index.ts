de22d2987792c078116643ff4c50e49e03e6c399
Stefan Schermann <stefan.schermann@gmail.com>
2020-06-12 19:54:47 +0200

    fix miniboards

--- src/ui/shared/svgboard/index.ts
+++ src/ui/shared/svgboard/index.ts
@@ -8,17 +8,19 @@ type BoardPos = [number, number]
 export function makeBoard(fen: string, orientation: Color, boardSize: cg.BoardSize) {
   const pieces = cgFen.read(fen)
   const piecesKey = Object.keys(pieces)
-  let b = '<svg xmlns="http://www.w3.org/2000/svg" width="360" height="360" viewBox="0 0 2100 2100">'
+  const width = 210 * boardSize[0], height = 210 * boardSize[1]
+  let b = '<svg xmlns="http://www.w3.org/2000/svg" width="360" height="360" viewBox="0 0 ' + width + ' ' + height + '">'
   for (let i = 0, len = piecesKey.length; i < len; i++) {
-    let pos = pos2px(orient(key2pos(piecesKey[i] as Key, boardSize), orientation))
+    let pos = pos2px(orient(key2pos(piecesKey[i] as Key, boardSize), orientation, boardSize))
     b += makePiece(pos, pieces[piecesKey[i]])
   }
   b += '</svg>'
   return b
 }
 
-function orient(pos: BoardPos, color: Color): BoardPos {
-  return color === 'white' ? pos : [6 - pos[0], 11 - pos[1]];
+function orient(pos: BoardPos, color: Color, boardSize: cg.BoardSize): BoardPos {
+  const w = (boardSize[0] / 2 + 1), h = boardSize[1] + 1
+  return color === 'white' ? pos : [w - pos[0], h - pos[1]];
 }
 
 function pos2px(pos: BoardPos): BoardPos {
@@ -27,7 +29,7 @@ function pos2px(pos: BoardPos): BoardPos {
 
 function makePiece(pos: BoardPos, piece: Piece) {
   const name = piece.color[0] + piece.role[0].toUpperCase();
-  return '<svg x="' + pos[0] + '" y="' + pos[1] + '" width="210" height="210">' +
-    svgPieces[name] +
+  return '<svg x="' + pos[0] + '" y="' + pos[1] + '" width="210" height="210">' + 
+    svgPieces[name] + 
     '</svg>'
 }
