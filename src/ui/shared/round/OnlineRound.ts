c2af2579974f770fd96d1d6966497d168673ac0f
Vincent Velociter <vincent.velociter@gmail.com>
2021-01-16 17:38:43 +0100

    Revert "Attempt to fix refreshing turns"
    This reverts commit a298b86a7ce3d5f0afa27f49c073fc926ee64408.

--- src/ui/shared/round/OnlineRound.ts
+++ src/ui/shared/round/OnlineRound.ts
@@ -162,8 +162,6 @@ export default class OnlineRound implements OnlineRoundInterface {
 
     this.transientMove = new TransientMove(this)
 
-    this.refreshMyTurns(0)
-
     redraw()
   }
 
@@ -529,20 +527,13 @@ export default class OnlineRound implements OnlineRoundInterface {
       }, 1)
     }
 
-    this.refreshMyTurns(5000)
-  }
-
-  private refreshMyTurns(delay: number) {
     if (!!this.data.opponent.ai && session.isConnected() && this.data.game.speed === 'correspondence') {
-      setTimeout(() => {
-        session.refresh()
-        .then(() => {
-          redraw()
-          if (Capacitor.platform === 'ios') {
-            Plugins.Badge.setNumber({ badge: session.myTurnGames().length })
-          }
-        })
-      }, delay)
+      session.refresh()
+      .then(() => {
+        if (Capacitor.platform === 'ios') {
+          Plugins.Badge.setNumber({ badge: session.myTurnGames().length })
+        }
+      })
     }
   }
 
