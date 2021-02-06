687ee7532339815fcc30e7eca455779252190a95
Vincent Velociter <vincent.velociter@gmail.com>
2021-02-06 15:14:22 +0100

    Tweak

--- src/ui/analyse/ceval/ScanClient.ts
+++ src/ui/analyse/ceval/ScanClient.ts
@@ -114,10 +114,10 @@ export default class ScanClient {
   private search = async () => {
     const work = this.startQueue.pop()
     if (work) {
+      this.work = work
+      this.curEval = undefined
       this.stopped = false
       this.startQueue = []
-      this.curEval = undefined
-      this.work = work
       this.ready = defer()
 
       await this.scan.send('pos pos=' + scanFen(work.initialFen) + (work.moves.length != 0 ? (' moves="' + work.moves.join(' ') + '"') : ''))
