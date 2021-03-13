8eac4ac316e8eb01fbe79f79965acba5d71479f7
Joe Stein <joeaarons@gmail.com>
2021-03-13 11:16:09 -0500

    Apply eslint to script/

--- scripts/i18n-update.ts
+++ scripts/i18n-update.ts
@@ -1,12 +1,12 @@
-import { readFileSync, createWriteStream, WriteStream, writeFileSync, fstat, existsSync, readdirSync, readdir, mkdirSync } from 'fs';
-import { parseStringPromise } from 'xml2js';
-import { get } from 'request';
-import { exec } from 'child_process';
-const colors = require('colors/safe');
+import { readFileSync, createWriteStream, WriteStream, writeFileSync, readdirSync, mkdirSync } from 'fs'
+import { parseStringPromise } from 'xml2js'
+import { get } from 'request'
+import { exec } from 'child_process'
+import colors = require('colors/safe')
 
-const baseDir = 'tmp/translations';
-const i18nBaseDir = '../www/i18n';
-const unzipMaxBufferSize = 1024 * 1024 * 10; // Set maxbuffer to 10MB to avoid errors when default 1MB used
+const baseDir = 'tmp/translations'
+const i18nBaseDir = '../www/i18n'
+const unzipMaxBufferSize = 1024 * 1024 * 10 // Set maxbuffer to 10MB to avoid errors when default 1MB used
 
 const modules = ['site', 'study', 'arena', 'perfStat', 'team', 'tfa']
 
@@ -14,22 +14,22 @@ type StringMap = {
   [i: string]: string | undefined
 }
 
-async function main(args: string[]) {
-  mkdirSync(`${baseDir}`, {recursive: true});
+async function main() {
+  mkdirSync(`${baseDir}`, {recursive: true})
 
   // Download translations zip
-  const zipFile = createWriteStream(`${baseDir}/out.zip`);
+  const zipFile = createWriteStream(`${baseDir}/out.zip`)
   downloadTranslationsTo(zipFile)
     .on('finish', async () => {
 
-    await unzipTranslations(`${baseDir}/out.zip`);
+    await unzipTranslations(`${baseDir}/out.zip`)
 
     const locales = readdirSync(`${baseDir}/dest`)
     .map(fn => fn.split('.')[0])
 
     // load and flatten translations in one object
     const everything = {}
-    for (const section of []) {
+    for (const section of modules) {
       const xml = await loadXml(locales, section)
       for (const lang in xml) {
         const locale = langToLocale(lang);
@@ -47,78 +47,78 @@ async function main(args: string[]) {
 
     const allKeys = Object.keys(everything)
 
-    writeFileSync(`${i18nBaseDir}/refs.js`, 'export default ' + JSON.stringify(allKeys, null, 2));
+    writeFileSync(`${i18nBaseDir}/refs.js`, 'export default ' + JSON.stringify(allKeys, null, 2))
     console.log(
       'Supported locales: ', allKeys.join(', ')
-    );
+    )
 
     // Write flattened translation objects to file. Skip if it would remove one or more keys.
     allKeys.forEach(locale => {
-      const newData = everything[locale];
+      const newData = everything[locale]
       try {
-        writeTranslations(`${i18nBaseDir}/${locale}.js`, newData);
+        writeTranslations(`${i18nBaseDir}/${locale}.js`, newData)
       } catch (e) {
-        console.error(colors.red(`Could not write translations for ${colors.bold(locale)}, skipping...`));
+        console.error(colors.red(`Could not write translations for ${colors.bold(locale)}, skipping...`))
       }
-    });
-  });
+    })
+  })
 }
 
 function downloadTranslationsTo(zipFile: WriteStream) {
-  console.log(colors.blue('Downloading translations...'));
+  console.log(colors.blue('Downloading translations...'))
   return get('https://crowdin.com/backend/download/project/lidraughts.zip')
     .pipe(zipFile)
     .on('finish', () => {
-      console.log(colors.green('  Download complete.'));
-    });
+      console.log(colors.green('  Download complete.'))
+    })
 }
 
 function unzipTranslations(zipFilePath: string) {
-  console.log(colors.blue('Unzipping translations...'));
+  console.log(colors.blue('Unzipping translations...'))
   return new Promise<void>((resolve, reject) => {
-      exec(`unzip -o ${zipFilePath} -d ${baseDir}/dest`, {maxBuffer: unzipMaxBufferSize}, (err, stdout, stderr) => {
+      exec(`unzip -o ${zipFilePath} -d ${baseDir}/dest`, {maxBuffer: unzipMaxBufferSize}, (err) => {
       if (err) {
-        return reject('Unzip failed.');
+        return reject('Unzip failed.')
       }
-      resolve();
-    });
-  });
+      resolve()
+    })
+  })
 }
 
 function loadTranslations(dir: string, locale: string) {
   return parseStringPromise(
     readFileSync(`${baseDir}/dest/${locale}/${dir}.xml`)
-  );
+  )
 }
 
 function unescape(str: string) {
-  return str.replace(/\\"/g, '"').replace(/\\'/g, '\'');
+  return str.replace(/\\"/g, '"').replace(/\\'/g, '\'')
 }
 
 function transformTranslations(data: any, locale: string, section: string): Promise<StringMap> {
-  console.log(colors.blue(`Transforming translations for ${colors.bold(locale)}...`));
-  const flattenedTranslations = {};
+  console.log(colors.blue(`Transforming translations for ${colors.bold(locale)}...`))
+  const flattenedTranslations = {}
 
   if (!(data && data.resources && data.resources.string)) {
-    return Promise.reject(`Missing translations in section ${section} and locale ${locale}`);
+    return Promise.reject(`Missing translations in section ${section} and locale ${locale}`)
   }
 
   data.resources.string.forEach((stringElement: any) => {
-    flattenedTranslations[stringElement.$.name] = unescape(stringElement._);
+    flattenedTranslations[stringElement.$.name] = unescape(stringElement._)
   });
 
   (data.resources.plurals || []).forEach((plural: any) => {
     plural.item.forEach((child: any) => {
-      flattenedTranslations[`${plural.$.name}:${child.$.quantity}`] = unescape(child._);
-    });
-  });
+      flattenedTranslations[`${plural.$.name}:${child.$.quantity}`] = unescape(child._)
+    })
+  })
 
-  return Promise.resolve(flattenedTranslations);
+  return Promise.resolve(flattenedTranslations)
 }
 
-function writeTranslations(where: string, data: object) {
-  console.log(colors.blue(`Writing to ${where}`));
-  writeFileSync(where, 'export default ' + JSON.stringify(data, null, 2));
+function writeTranslations(where: string, data: any) {
+  console.log(colors.blue(`Writing to ${where}`))
+  writeFileSync(where, 'export default ' + JSON.stringify(data, null, 2))
 }
 
 function langToLocale(lang: string): string {
@@ -177,4 +177,4 @@ async function loadXml(locales: readonly string[], section: string) {
   return sectionXml
 }
 
-main(process.argv);
+main()
