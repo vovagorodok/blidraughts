89f231095bd59aa1f0b54c6481d8a9bc8ce8e4c3
Vincent Velociter <vincent.velociter@gmail.com>
2021-08-07 14:09:47 +0200

    Now able to run android with cap3

---
+++ capacitor.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="@capacitor/splash-screen" />
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'org.lidraughts.mobileapp',
+  appName: 'lidraughts',
+  bundledWebRuntime: false,
+  webDir: 'www',
+  backgroundColor: '000000ff',
+  appendUserAgent: 'Lidrobile/7.0.5',
+  plugins: {
+    SplashScreen: {
+      androidSplashResourceName: 'launch_splash',
+      launchAutoHide: false,
+      useDialog: false,
+    },
+    PushNotifications: {
+      presentationOptions: ['sound', 'alert']
+    }
+  }
+}
+
+export default config
