53980b68eea8462666bb82f9e52a63babeeb3e79
Stefan Schermann <stefan.schermann@gmail.com>
2023-08-27 00:11:29 +0200

    Revert "Remove patron links on ios"
    This reverts commit 762ae01b9755e1654b34aef1e462b5d23483757d.

--- src/ui/about.tsx
+++ src/ui/about.tsx
@@ -33,6 +33,7 @@ export default {
           <li>{externalLink('Contact', 'https://lidraughts.org/contact')}</li>
           <li>{externalLink('Terms of Service', 'https://lidraughts.org/terms-of-service')}</li>
           <li>{externalLink('Privacy Policy', 'https://lidraughts.org/privacy')}</li>
+          <li>{externalLink('lidraughts.org/about', 'https://lidraughts.org/about')}</li>
         </ul>
 
       </div>
