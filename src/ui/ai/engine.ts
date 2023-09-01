Mageek627/lichobile

Typesafe stockfish plugin

src/ui/ai/engine.ts
@@ -1,6 +1,5 @@
-import { Plugins } from '@capacitor/core'
 import { AiRoundInterface } from '../shared/round'
-import { send, getNbCores, setOption, setVariant } from '../../utils/stockfish'
+import { Stockfish, send, getNbCores, setOption, setVariant } from '../../stockfish'
 
 interface LevelToDepth {
   [index: number]: number
@@ -33,7 +32,7 @@ export default function(ctrl: AiRoundInterface): EngineInterface {
 
   return {
     init() {
-      Plugins.Stockfish.addListener('output', ({ line }: { line: string }) => {
+      Stockfish.addListener('output', ({ line }) => {
         console.debug('[stockfish >>] ' + line)
         const match = line.match(/^bestmove (\w{4})|^bestmove ([PNBRQ]@\w{2})/)
         if (match) {
@@ -42,7 +41,7 @@ export default function(ctrl: AiRoundInterface): EngineInterface {
         }
       })
 
-      return Plugins.Stockfish.start()
+      return Stockfish.start()
       .then(onInit)
       .catch(console.error.bind(console))
     },
@@ -67,8 +66,8 @@ export default function(ctrl: AiRoundInterface): EngineInterface {
     },
 
     exit() {
-      Plugins.Stockfish.removeAllListeners()
-      return Plugins.Stockfish.exit()
+      Stockfish.removeAllListeners()
+      return Stockfish.exit()
     }
   }
 }
@@ -77,7 +76,7 @@ function onInit() {
   return send('uci')
   .then(() => setOption('Threads', getNbCores()))
   .then(async () => {
-    const { value: mem }= await Plugins.Stockfish.getMaxMemory()
+    const { value: mem } = await Stockfish.getMaxMemory()
     setOption('Hash', mem)
   })
   .then(() => setOption('Ponder', 'false'))

src/ui/analyse/analyseSettings.ts
@@ -5,8 +5,8 @@ import router from '../../router'
 import * as gameApi from '../../lichess/game'
 import { isOnlineAnalyseData } from '../../lichess/interfaces/analyse'
 import settings from '../../settings'
+import { getNbCores } from '../../stockfish'
 import { oppositeColor } from '../../utils'
-import { getNbCores } from '../../utils/stockfish'
 import formWidgets from '../shared/form'
 import AnalyseCtrl from './AnalyseCtrl'
 

src/ui/analyse/ceval/CevalCtrl.ts
@@ -1,7 +1,6 @@
-import { Plugins } from '@capacitor/core'
 import settings from '../../../settings'
 import { Tree } from '../../shared/tree'
-import { getNbCores } from '../../../utils/stockfish'
+import { Stockfish, getNbCores } from '../../../stockfish'
 import StockfishEngine from './StockfishEngine'
 import { Opts, Work, ICevalCtrl } from './interfaces'
 
@@ -10,7 +9,7 @@ export default function CevalCtrl(
   emit: (path: string, res?: Tree.ClientEval) => void,
 ): ICevalCtrl {
 
-  const hashPromise = Plugins.Stockfish.getMaxMemory()
+  const hashPromise = Stockfish.getMaxMemory()
 
   let initialized = false
 

vovagorodok/blidraughts

src/ui/ai/engine.ts
@@ -1,6 +1,5 @@
-import { Plugins } from '@capacitor/core'
 import { AiRoundInterface } from '../shared/round'
-import { send, getNbCores, setOption, parsePV, parseVariant, scanFen } from '../../utils/scan'
+import { Scan, send, getNbCores, setOption, parsePV, parseVariant, scanFen } from '../utils/scan'
 
 interface LevelToNumber {
   [index: number]: number
@@ -64,7 +63,7 @@ export default function(ctrl: AiRoundInterface): EngineInterface {
     init(v: VariantKey) {
       initVariant = v
 
-      Plugins.Scan.addListener('output', ({ line }: { line: string }) => {
+      Scan.addListener('output', ({ line }) => {
         console.debug('[scan >>] ' + line)
         const match = line.match(/^done move=([0-9\-xX\s]+)/)
         if (match) {
@@ -72,7 +71,7 @@ export default function(ctrl: AiRoundInterface): EngineInterface {
         }
       })
 
-      return Plugins.Scan.start(parseVariant(initVariant))
+      return Scan.start(parseVariant(initVariant))
         .then(onInit)
         .catch(console.error.bind(console))
     },
@@ -141,8 +140,8 @@ export default function(ctrl: AiRoundInterface): EngineInterface {
     },
 
     exit() {
-      Plugins.Scan.removeAllListeners()
-      return Plugins.Scan.exit()
+      Scan.removeAllListeners()
+      return Scan.exit()
     },
 
     variant() { 
@@ -156,7 +155,7 @@ function onInit() {
     .then(() => send('init'))
     .then(() => setOption('threads', getNbCores()))
     .then(async () => {
-      const { value: mem }= await Plugins.Scan.getMaxMemory()
+      const { value: mem } = await Scan.getMaxMemory()
       setOption('hash', mem)
     })
 }

src/ui/analyse/analyseSettings.ts
@@ -5,8 +5,8 @@ import router from '../../router'
 import * as gameApi from '../../lidraughts/game'
 import { isOnlineAnalyseData } from '../../lidraughts/interfaces/analyse'
 import settings from '../../settings'
+import { getNbCores } from '../../scan'
 import { oppositeColor } from '../../utils'
-import { getNbCores } from '../../utils/scan'
 import formWidgets from '../shared/form'
 import AnalyseCtrl from './AnalyseCtrl'
 

src/ui/analyse/ceval/CevalCtrl.ts
@@ -1,7 +1,6 @@
-import { Plugins } from '@capacitor/core'
 import settings from '../../../settings'
 import { Tree } from '../../shared/tree'
-import { getNbCores } from '../../../utils/scan'
+import { Scan, getNbCores } from '../../../scan'
 import ScanEngine from './ScanEngine'
 import { Opts, Work, ICevalCtrl } from './interfaces'
 
@@ -10,7 +9,7 @@ export default function CevalCtrl(
   emit: (path: string, res?: Tree.ClientEval) => void,
 ): ICevalCtrl {
 
-  const hashPromise = Plugins.Stockfish.getMaxMemory()
+  const hashPromise = Scan.getMaxMemory()
 
   let initialized = false
 