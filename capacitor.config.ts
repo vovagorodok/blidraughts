c52fea312623665e3df3fbf24a50735b47d9e18d
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2025-10-07 23:05:04 +0200

    Changed ver to ble2.2.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,7 +7,7 @@ const config: CapacitorConfig = {
   bundledWebRuntime: false,
   webDir: 'www',
   backgroundColor: '000000ff',
-  appendUserAgent: 'Lichobile/8.0.0+ble2.1.2',
+  appendUserAgent: 'Lichobile/8.0.0+ble2.2.0',
   plugins: {
     SplashScreen: {
       androidSplashResourceName: 'launch_splash',
