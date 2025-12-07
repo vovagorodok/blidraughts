362ee36745d5554255d458f096e778478ef25707
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2025-12-07 04:01:06 +0100

    Update version to 2.4.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,7 +7,7 @@ const config: CapacitorConfig = {
   bundledWebRuntime: false,
   webDir: 'www',
   backgroundColor: '000000ff',
-  appendUserAgent: 'Lichobile/8.0.0+ble2.3.1',
+  appendUserAgent: 'Lichobile/8.0.0+ble2.4.0',
   plugins: {
     SplashScreen: {
       androidSplashResourceName: 'launch_splash',
