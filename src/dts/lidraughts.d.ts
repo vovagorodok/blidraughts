5de1b26ae658ac8e18a782753bc3d526ae429a62
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2025-12-18 10:19:49 +0100

    Fixed ChessKey and start from a1

--- src/dts/lidraughts.d.ts
+++ src/dts/lidraughts.d.ts
@@ -29,7 +29,6 @@ declare type KeyPair = [Key, Key]
 declare type ChessRank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
 declare type ChessFile = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j'
 declare type ChessKey =
-  | 'a0' | 'b0' | 'c0' | 'd0' | 'e0' | 'f0' | 'g0' | 'h0' | 'i0' | 'j0'
   | 'a1' | 'b1' | 'c1' | 'd1' | 'e1' | 'f1' | 'g1' | 'h1' | 'i1' | 'j1'
   | 'a2' | 'b2' | 'c2' | 'd2' | 'e2' | 'f2' | 'g2' | 'h2' | 'i2' | 'j2'
   | 'a3' | 'b3' | 'c3' | 'd3' | 'e3' | 'f3' | 'g3' | 'h3' | 'i3' | 'j3'
@@ -38,7 +37,8 @@ declare type ChessKey =
   | 'a6' | 'b6' | 'c6' | 'd6' | 'e6' | 'f6' | 'g6' | 'h6' | 'i6' | 'j6'
   | 'a7' | 'b7' | 'c7' | 'd7' | 'e7' | 'f7' | 'g7' | 'h7' | 'i7' | 'j7'
   | 'a8' | 'b8' | 'c8' | 'd8' | 'e8' | 'f8' | 'g8' | 'h8' | 'i8' | 'j8'
-  | 'a9' | 'b9' | 'c9' | 'd9' | 'e9' | 'f9' | 'g9' | 'h9' | 'i9' | 'j9';
+  | 'a9' | 'b9' | 'c9' | 'd9' | 'e9' | 'f9' | 'g9' | 'h9' | 'i9' | 'j9'
+  | 'a:' | 'b:' | 'c:' | 'd:' | 'e:' | 'f:' | 'g:' | 'h:' | 'i:' | 'j:';
 
 declare type ChessKeyPair = [ChessKey, ChessKey]
 
